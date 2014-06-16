43a5ff7b29e5fc9e986d627d879a8da493fb0afa
Ramy Eldesoky <rdesoky@gmail.com>
2014-06-16 13:15:26 -0700

    Implement promises join feature

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 JSPromise
 =========
-Require less than 100 lines of javascript code to utilize Promise class for implementing asynchronous processes with chaining capability.
+Require about 100 lines of javascript code to utilize Promise class for implementing asynchronous processes with chaining and joining capabilities.
 
 package.json
 ------------
@@ -36,6 +36,20 @@ myTimeout(3000)
     })
     .done(function(delay){
         console.log("Done " + delay + " ms!");
+        testJoin();
     });
+
+function testJoin() {
+    console.log("Waiting for 3 joined promises to finishes in 5 seconds on:" + (new Date()).toISOString());
+
+    JSPromise.join([
+        myTimeout(2000),
+        myTimeout(1000),
+        myTimeout(5000),
+    ]).done(function(){
+        console.log("Done on: " + (new Date()).toISOString());
+        console.log("Test Passed!");
+    })
+}
 
 ```
--- test/index.js
+++ test/index.js
@@ -12,16 +12,28 @@ function myTimeout(ms){
 
 console.log("Waiting ... ");
 
-myTimeout(3000)
+myTimeout(1000)
     .then(function(delay){
         console.log("Done " + delay + " ms!");
         return myTimeout(2000);
     })
     .then(function(delay){
         console.log("Done " + delay + " ms!");
-        return myTimeout(1000);
+        return myTimeout(3000);
     })
     .done(function(delay){
         console.log("Done " + delay + " ms!");
-        console.log("Test Passed!");
+        testJoin();
     });
+
+function testJoin() {
+    console.log("Waiting for 3 joined promises to finishes in 5 seconds on:" + (new Date()).toISOString());
+    JSPromise.join([
+        myTimeout(2000),
+        myTimeout(1000),
+        myTimeout(5000),
+    ]).done(function(){
+        console.log("Done on: " + (new Date()).toISOString());
+        console.log("Test Passed!");
+    })
+}
